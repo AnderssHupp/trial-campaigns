c9102ffc71f49ead245965263ea026cb220c647a
AnderssHupp <anderssonhupp@gmail.com>
2026-03-16 22:36:52 +0100

    criação das rotas api restful

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,20 @@
 <?php
 
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ContactListController;
+use App\Http\Controllers\CampaignController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('contacts', [ContactController::class, 'index'])->name('contacts.index');
+Route::post('contacts', [ContactController::class, 'store'])->name('contacts.store');
+Route::post('contacts/{id}/unsubscribe', [ContactController::class, 'unsubscribe'])->name('contacts.unsubscribe');
+
+Route::get('/contact-lists', [ContactListController::class, 'index'])->name('contactsList.index');
+Route::post('/contact-lists', [ContactListController::class, 'store'])->name('contactsList.store');
+Route::post('/contact-lists/{id}/contacts', [ContactListController::class, 'addContact'])->name('contactsList.addContact');
+
+
+Route::get('/campaigns', [CampaignController::class, 'index'])->name('campaign.index');
+Route::post('/campaigns', [CampaignController::class, 'store'])->name('campaign.store');
+Route::get('/campaigns/{id}', [CampaignController::class, 'show'])->name('campaign.show');
+Route::post('/campaigns/{id}/dispatch', [CampaignController::class, 'dispatch'])->name('campaign.dispatch');
