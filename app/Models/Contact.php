7fb1748e6b12adf0d1091341e67e39832c49c566
AnderssHupp <anderssonhupp@gmail.com>
2026-03-16 22:40:09 +0100

    fix(Contact) adicionado importa has factory

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -3,17 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Contact extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'name',
         'email',
         'status',
     ];
-    protected $casts = [
-        'status' => 'enum:active,unsubscribed',
-    ];
+
     protected $table = 'contacts';
 
     public function contactLists()
