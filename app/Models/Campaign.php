7cd38b6ec70338be9349edb9d9a7300a4a6247fe
AnderssHupp <anderssonhupp@gmail.com>
2026-03-16 14:05:15 +0100

    created campaing  model

--- app/Models/Campaign.php
+++ app/Models/Campaign.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\ContactList;
+use App\Models\CampaignSend;
 
 class Campaign extends Model
 {
