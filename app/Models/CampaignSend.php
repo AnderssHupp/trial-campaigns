6daddbb20c4aebf2d411d8525a5f8abd81a42c40
AnderssHupp <anderssonhupp@gmail.com>
2026-03-16 22:40:49 +0100

    fix(CampaignSend) adicionado importe has factory

--- app/Models/CampaignSend.php
+++ app/Models/CampaignSend.php
@@ -3,16 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class CampaignSend extends Model
 {
+    use HasFactory;
+    
     protected $fillable = [
         'campaign_id',
         'contact_id',
         'status',
     ];
     protected $casts = [
-        'status' => 'enum:pending,sent,failed',
+        'status' => 'string',
     ];
     protected $table = 'campaign_sends';
     
